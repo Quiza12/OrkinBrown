968ea7ecc4e4e233bde436633d0c0cebe0807a34
Quiza12 <Quiza12@live.com>
2025-01-19 20:23:41 +1100

    Staff positions.

--- src/app/data/job-cards.service.ts
+++ src/app/data/job-cards.service.ts
@@ -1,78 +1,84 @@
 import { Job } from "../classes/job";
 
 export class JobCards {
-
     auditJobCards: Job[] = [
-        new Job('Audit Junior',
-        'Johannesburg Office (not remote)',
-        '3-5 years experience',
-        [
-            'Standards of accounting - IRBA certified',
-            'Knowledge of regulatory standards',
-            'Software proficiency',
-            'Attention to detail',
-            'Critical thinking'
-        ],
-        'Competitive rates'),
-        new Job('Audit Intermediary',
-        'Johannesburg Office (not remote)',
-        '3-5 years experience',
-        [
-            'Standards of accounting - IRBA certified',
-            'Knowledge of regulatory standards',
-            'Software proficiency',
-            'Attention to detail',
-            'Critical thinking'
-        ],
-        'Competitive rates'),
+        new Job('Audit Manager',
+            'Johannesburg Office (not remote)',
+            'Small to medium sized audit firm experience',
+            [
+                'SAICA recognised qualifications',
+                'Ability to manage and take responsibility for a portfolio of clients',
+                'Experience in reporting directly to partners and dealing directly with clients',
+                'Experience in managing and supervising audit teams',
+                'Must have audited clients within diverse industries in the private sector',
+                'Caseware experience',
+                'Experience in completing and submitting Personal, Company and Trust income tax returns and calculating provisional tax'
+            ],
+            'Salary dependant on qualifications and experience'),
         new Job('Audit Senior',
             'Johannesburg Office (not remote)',
-            '3-5 years experience',
+            'Small to medium sized audit firm experience',
             [
-                'Standards of accounting - IRBA certified',
-                'Knowledge of regulatory standards',
-                'Software proficiency',
-                'Attention to detail',
-                'Critical thinking'
+                'SAICA recognised qualifications',
+                'Experience in reporting directly to partners and dealing directly with clients',
+                'Experience in managing and supervising audit teams',
+                'Must have audited clients within diverse industries in the private sector',
+                'Caseware experience',
+                'Experience in completing and submitting Personal, Company and Trust income tax returns and calculating provisional tax',
             ],
-            'Competitive rates'),
-        new Job('Audit Manager',
+            'Salary dependant on qualifications and experience'),
+        new Job('Audit Intermediary',
+            'Johannesburg Office (not remote)',
+            '2-3 years experience in small to medium sized audit firm',
+            [
+                'Ability to conduct small audits and Independent reviews',
+                'Caseware experience',
+                'Experience in completing and submitting Personal and Company income tax returns'
+            ],
+            'Salary dependant on qualifications and experience'),
+        new Job('Audit Junior',
         'Johannesburg Office (not remote)',
-        '3-5 years experience',
+        '',
         [
-            'Standards of accounting - IRBA certified',
-            'Knowledge of regulatory standards',
-            'Software proficiency',
-            'Attention to detail',
-            'Critical thinking'
+            'Seeking to become Chartered Accountants',
+            'Wanting to enter or continue with SAICA training contacts',
+            'Candidates are required to study part-time while completing SAICA training contract',
+            'Own transport essential',
         ],
-        'Competitive rates'),
+        ''),
     ]
 
     taxationJobCards: Job[] = [
-        new Job('Taxation Assistant',
+        new Job('Junior Administrator',
             'Johannesburg Office (not remote)',
-            '3-5 years experience',
+            'Matriculant',
             [
-                'Standards of accounting - IRBA certified',
-                'Knowledge of regulatory standards',
-                'Software proficiency',
-                'Attention to detail',
-                'Critical thinking'
+                'Good communication skills',
+                'Good organisational skills',
+                'Attention to detail'
             ],
-            'Competitive rates'),
+            ''),
         new Job('Taxation Consultant',
             'Johannesburg Office (not remote)',
-            '3-5 years experience',
+            'Experience in a tax department at small to medium sized audit firm',
             [
-                'Standards of accounting - IRBA certified',
-                'Knowledge of regulatory standards',
-                'Software proficiency',
-                'Attention to detail',
-                'Critical thinking'
+                'Taxware and SARS e-filing experience',
+                'Good communication skills',
+                'Experience in dealing with tax audits and objections'
             ],
-            'Competitive rates')
+            'Salary dependant on relevant experience')
     ]
+
+    
+
+
+ 
+
+
+    
+    
+    
+    
 
 }
 
