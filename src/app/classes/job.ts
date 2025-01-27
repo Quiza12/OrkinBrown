6be1d23e1f1cc04f17e318cce84bb003b0c4572e
Quiza12 <Quiza12@live.com>
2025-01-27 14:40:29 +1100

    Updates to career cards and minor home page bug fix.

--- src/app/classes/job.ts
+++ src/app/classes/job.ts
@@ -1,16 +1,10 @@
 export class Job {
     title: string;
-    location: string;
-    experience: string;
     skills: string[];
-    pay: string;
   
-    constructor(title: string, location: string, experience: string, skills: string[], pay: string) {
+    constructor(title: string, skills: string[]) {
         this.title = title;
-        this.location = location;
-        this.experience = experience;
         this.skills = skills;
-        this.pay = pay;
     }
   }
   
